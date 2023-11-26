fcf900c9e0b8e9ab78cc861624fa7676085beda2
Sarath <vssarathc04@gmail.com>
2023-11-26 21:12:28 +0530

    fmt changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,21 @@
 <script lang="ts">
-  import { useForm, validators, HintGroup, Hint, email, required, type Validator } from "svelte-use-form";
+  import {
+    useForm,
+    validators,
+    HintGroup,
+    Hint,
+    email,
+    required,
+    type Validator,
+  } from "svelte-use-form";
 
   const rollNumberValidator: Validator = (value: string) => {
     const pattern = new RegExp(/\d\d\w\w\w\d\d\d\d/);
-    console.log(pattern.test(value))
+    console.log(pattern.test(value));
     if (!pattern.test(value)) {
-      return {"Invalid roll number": "Please enter a valid roll number"};
+      return { "Invalid roll number": "Please enter a valid roll number" };
     }
-  };     
+  };
 
   const form = useForm();
 </script>
@@ -16,22 +24,31 @@
   <h1>Login</h1>
 
   <label for="roll-number">Email: </label>
-  <input type="text" name="roll-number" use:validators={[required, rollNumberValidator]} placeholder="Enter your registration number"/>
+  <input
+    type="text"
+    name="roll-number"
+    use:validators={[required, rollNumberValidator]}
+    placeholder="Enter your registration number"
+  />
 
   <label for="password">Password: </label>
-  <input type="password" name="password" use:validators={[required]} placeholder="Enter your password"/>
+  <input
+    type="password"
+    name="password"
+    use:validators={[required]}
+    placeholder="Enter your password"
+  />
 
-  <div> 
+  <div>
+    <HintGroup for="roll-number">
+      <Hint on="required">This is a mandatory field</Hint>
+      <Hint on="Invalid roll number">Please enter a valid roll number</Hint>
+    </HintGroup>
 
-  <HintGroup for="roll-number">
-    <Hint on="required">This is a mandatory field</Hint>
-    <Hint on="Invalid roll number">Please enter a valid roll number</Hint>
-  </HintGroup>
-
-  <HintGroup for="password">
-    <Hint on="required">This is a mandatory field</Hint>
-  </HintGroup>
-    </div>
+    <HintGroup for="password">
+      <Hint on="required">This is a mandatory field</Hint>
+    </HintGroup>
+  </div>
 
   <button disabled={!$form.valid}>Login</button>
 </form>
@@ -40,10 +57,9 @@
 <!-- {JSON.stringify($form, null, " ")} -->
 <!-- </pre> -->
 
-  <style>
-	:global(.touched:invalid) {
-		border-color: red;
-		outline-color: red;
-	}
+<style>
+  :global(.touched:invalid) {
+    border-color: red;
+    outline-color: red;
+  }
 </style>
-
