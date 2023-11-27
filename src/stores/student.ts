64bc0634634735c3dd453d3bc5dc198259fadc9c
Sarath <vssarathc04@gmail.com>
2023-11-27 20:25:05 +0530

    +load student data from local cache fn

--- src/stores/student.ts
+++ src/stores/student.ts
@@ -2,6 +2,7 @@ import { writable } from "svelte/store";
 import type { Student } from "../api/allDetails";
 
 export const studentData = writable<Student | null>(null);
+loadStudentDataFromCache();
 
 // write to local cache
 studentData.subscribe((value) => {
@@ -10,8 +11,10 @@ studentData.subscribe((value) => {
   }
 });
 
-// read from local cache
-const cachedStudent = localStorage.getItem("studentData-V1");
-if (cachedStudent) {
-  studentData.set(JSON.parse(cachedStudent));
+function loadStudentDataFromCache() {
+  // read from local cache
+  const cachedStudent = localStorage.getItem("studentData-V1");
+  if (cachedStudent) {
+    studentData.set(JSON.parse(cachedStudent));
+  }
 }
