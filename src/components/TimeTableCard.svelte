21da575044451d45156ecc6764e71e624a258ef0
Sarath <vssarathc04@gmail.com>
2023-11-27 20:22:16 +0530

    simple time table card component

---
+++ src/components/TimeTableCard.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+import type { TimeSlot } from "../api/allDetails";
+export let singleSlotEntry: TimeSlot;
+</script>
+
+<div class="my-2 flex flex-col overflow-hidden rounded-lg bg-white p-6 shadow">
+  <p class="text-sm font-medium text-indigo-600">
+    {singleSlotEntry.courseName}
+  </p>
+  <a href="#" class="mt-2 block">
+    <p class="text-xl font-semibold text-gray-900">
+      {singleSlotEntry.startTime} - {singleSlotEntry.endTime}
+    </p>
+    <p class="mt-3 text-base text-gray-500">
+      {singleSlotEntry.class}
+    </p>
+  </a>
+</div>
