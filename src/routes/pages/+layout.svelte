0b7b667ceb320584bca6228973d698bb375aaee2
Sarath <vssarathc04@gmail.com>
2023-11-27 20:22:55 +0530

    +simple bottom routing handler

---
+++ src/routes/pages/+layout.svelte
@@ -0,0 +1,75 @@
+<slot/>
+<!-- Create a bottom navigatoion bar in svelte and tailwind the bottom nav bar is fixed to the bottom of the screen-->
+<div class="fixed bottom-0 w-full bg-white shadow">
+  <div class="flex justify-around">
+    <a href="/pages/home" class="flex flex-col items-center justify-center py-2">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="h-6 w-6"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="#000000"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+        />
+      </svg>
+      <span class="text-xs">Home</span>
+    </a>
+    <a href="/pages/attendance" class="flex flex-col items-center justify-center py-2">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="h-6 w-6"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="#000000"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+        />
+      </svg>
+      <span class="text-xs">Attendance</span>
+    </a>
+    <a href="/pages/timetable" class="flex flex-col items-center justify-center py-2">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="h-6 w-6"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="#000000"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+        />
+      </svg>
+      <span class="text-xs">Timetable</span>
+    </a>
+    <a href="/pages/profile" class="flex flex-col items-center justify-center py-2">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="h-6 w-6"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="#000000"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+        />
+      </svg>
+      <span class="text-xs">Profile</span>
+    </a>
+  </div>
+</div>
+
