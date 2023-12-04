07039ec6df6a8c0e900bc099cd6f14e6e178f106
Sarath <vssarathc04@gmail.com>
2023-12-04 18:20:06 +0530

    no timetable msg for no data

--- src/routes/pages/timetable/+page.svelte
+++ src/routes/pages/timetable/+page.svelte
@@ -1,21 +1,35 @@
 <script lang="ts">
 import { studentData } from "../../../stores/student";
-import type { TimeTable, TimeSlot } from "../../../api/allDetails";
+import type { TimeTable } from "../../../api/allDetails";
 import TimeTableCard from "../../../components/TimeTableCard.svelte";
 
 let timeTable: TimeTable = $studentData?.timetable;
-$: timeTableAsEntries = Object.entries<TimeSlot[]>(timeTable ?? {});
+
+// make the timeTable variable undefined if the timeTable is empty
+if (timeTable !== undefined && timeTable !== null) {
+  let isEmpty = true;
+  let sum = 0
+  for (let dayName in timeTable) {
+    sum += timeTable[dayName].length;
+  }
+  isEmpty = sum === 0;
+  if (isEmpty) {
+    timeTable = undefined;
+  }
+}
 </script>
 
 <!-- Create a scrollable card container with the timetable  -->
-{#if timeTable !== undefined && timeTable !== null}
+{#if timeTable !== undefined && timeTable !== null }
   {#each ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"] as dayName}
-    {#if timeTable[dayName].length != 0}
+    {#if timeTable[dayName] !== undefined && timeTable[dayName].length != 0}
       <h1 class="text-5xl">{dayName}</h1>
       {#each timeTable[dayName] as singleSlotEntry}
         <!-- Create a card style component using tailwind-->
-        <TimeTableCard singleSlotEntry="{singleSlotEntry}" />
+        <TimeTableCard singleSlotEntry="{singleSlotEntry}"/>
       {/each}
     {/if}
   {/each}
+{:else}
+  <h1 class="text-5xl">No time-table found !</h1>
 {/if}
