0ec36b1b07c7988f85ef8841e868d58a9cbe744c
Sarath <vssarathc04@gmail.com>
2023-12-04 18:18:44 +0530

    No data msg for null data in acad history

--- src/routes/pages/academic_history/+page.svelte
+++ src/routes/pages/academic_history/+page.svelte
@@ -10,8 +10,8 @@ const sortedSummaryKeys = [
   "E",
   "F",
   "CGPA",
-  "Credits Earned",
-  "Credits Registered",
+  "CreditsEarned",
+  "CreditsRegistered",
 ];
 </script>
 
@@ -19,16 +19,17 @@ const sortedSummaryKeys = [
   <h1 class="text-5xl">No Academic History Found</h1>
 {:else}
   {#if acadHistory.summary !== undefined && acadHistory.summary !== null}
-    <h1 class="text-5xl">Summary</h1>
-    {#each Object.entries(acadHistory.summary) as [summaryName, summaryValue]}
+    <h1 class="mb-2 text-5xl">Summary</h1>
+    {#each sortedSummaryKeys as summaryKey}
       <div class="flex flex-row justify-between">
-        <h1 class="text-base">{summaryName}</h1>
-        <h1 class="text-sm">{summaryValue}</h1>
+        <h1 class="text-base">{summaryKey}</h1>
+        <h1 class="text-sm">{acadHistory.summary[summaryKey]}</h1>
       </div>
     {/each}
   {/if}
+  <div class="m-5"></div>
   {#if acadHistory.subjects !== undefined && acadHistory.subjects !== null}
-    <h1 class="text-5xl">Academic History</h1>
+    <h1 class="mb-2 text-5xl">Academic History</h1>
     {#each Object.entries(acadHistory.subjects) as [subjectName, gradeAchieved]}
       <div class="flex flex-row justify-between">
         <h1 class="text-base">{subjectName}</h1>
