49e30e0cfe65f166274977d5b4f6383f5c2adcd2
Sarath <vssarathc04@gmail.com>
2023-12-04 17:30:55 +0530

    small lint fixed

---
+++ src/routes/pages/academic_history/+page.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+import { studentData } from "../../../stores/student";
+const acadHistory = $studentData?.academic_history;
+const sortedSummaryKeys = [
+  "S",
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "CGPA",
+  "Credits Earned",
+  "Credits Registered",
+];
+</script>
+
+{#if acadHistory === undefined || acadHistory === null}
+  <h1 class="text-5xl">No Academic History Found</h1>
+{:else}
+  {#if acadHistory.summary !== undefined && acadHistory.summary !== null}
+    <h1 class="text-5xl">Summary</h1>
+    {#each Object.entries(acadHistory.summary) as [summaryName, summaryValue]}
+      <div class="flex flex-row justify-between">
+        <h1 class="text-base">{summaryName}</h1>
+        <h1 class="text-sm">{summaryValue}</h1>
+      </div>
+    {/each}
+  {/if}
+  {#if acadHistory.subjects !== undefined && acadHistory.subjects !== null}
+    <h1 class="text-5xl">Academic History</h1>
+    {#each Object.entries(acadHistory.subjects) as [subjectName, gradeAchieved]}
+      <div class="flex flex-row justify-between">
+        <h1 class="text-base">{subjectName}</h1>
+        <h1 class="text-sm">{gradeAchieved}</h1>
+      </div>
+    {/each}
+  {/if}
+{/if}
