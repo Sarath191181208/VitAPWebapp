9062da27a805a0756efdf1c9bcf791419d0dfd29
Sarath <vssarathc04@gmail.com>
2023-12-04 18:17:48 +0530

    +auto-margin for screen

--- src/routes/pages/+layout.svelte
+++ src/routes/pages/+layout.svelte
@@ -1,4 +1,6 @@
+<div class="p-5 pb-20 max-w-xl mx-auto">
 <slot/>
+</div>
 <!-- Create a bottom navigatoion bar in svelte and tailwind the bottom nav bar is fixed to the bottom of the screen-->
 <div class="fixed bottom-0 w-full bg-white shadow">
   <div class="flex justify-around">
@@ -55,7 +57,7 @@
     </a>
     <a href="/pages/profile" class="flex flex-col items-center justify-center py-2">
       <svg
-        xmlns="http://www.w3.org/2000/svg"
+        xmlns="http://www.w3.org/2000/svg"
         class="h-6 w-6"
         fill="none"
         viewBox="0 0 24 24"
@@ -69,6 +71,24 @@
         />
       </svg>
       <span class="text-xs">Profile</span>
+    </a>
+
+  <a href="/pages/academic_history" class="flex flex-col items-center justify-center py-2">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="h-6 w-6"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="#000000"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+        />
+      </svg>
+      <span class="text-xs">Academic History</span>
     </a>
   </div>
 </div>
