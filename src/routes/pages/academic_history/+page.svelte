2471da702ac42f7f7e991e011f4f7de947b7db0a
Sarath <vssarathc04@gmail.com>
2023-12-05 13:12:16 +0530

    academic_history icon added

--- src/routes/pages/academic_history/+page.svelte
+++ src/routes/pages/academic_history/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+import AcademicsIcon from "../../../components/icons/AcademicsIcon.svelte";
 import SettingsIcon from "../../../components/icons/SettingsIcon.svelte";
 import { studentData } from "../../../stores/student";
 const acadHistory = $studentData?.academic_history;
@@ -16,27 +17,29 @@ const sortedSummaryKeys = [
 ];
 </script>
 
-  <h1
+<h1
   class="bg-gradient-to-r from-rose-500 to-amber-500 bg-clip-text pb-4 text-5xl font-bold text-transparent"
 >
-  <SettingsIcon className="text-3xl" /> Academics  Page
+  <AcademicsIcon className="text-5xl" /> Academics Page
 </h1>
 
 {#if acadHistory === undefined || acadHistory === null}
   <h1 class="text-5xl dark:text-white">No Academic History Found</h1>
 {:else}
   {#if acadHistory.summary !== undefined && acadHistory.summary !== null}
-    <h1 class="mb-2 text-5xl dark:text-white">Summary</h1>
+    <h1 class="mb-2 text-2xl dark:text-white">Summary</h1>
     {#each sortedSummaryKeys as summaryKey}
       <div class="flex flex-row justify-between">
         <h1 class="text-base dark:text-white">{summaryKey}</h1>
-        <h1 class="text-sm dark:text-white">{acadHistory.summary[summaryKey]}</h1>
+        <h1 class="text-sm dark:text-white">
+          {acadHistory.summary[summaryKey]}
+        </h1>
       </div>
     {/each}
   {/if}
   <div class="m-5"></div>
   {#if acadHistory.subjects !== undefined && acadHistory.subjects !== null}
-    <h1 class="mb-2 text-5xl dark:text-white">Academic History</h1>
+    <h1 class="mb-2 text-3xl dark:text-white">Academic History</h1>
     {#each Object.entries(acadHistory.subjects) as [subjectName, gradeAchieved]}
       <div class="flex flex-row justify-between">
         <h1 class="text-base dark:text-white">{subjectName}</h1>
