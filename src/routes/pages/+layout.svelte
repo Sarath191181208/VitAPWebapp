c2973a4d32cc8fb94cab80d5cca5e9a2579a93ca
Sarath <vssarathc04@gmail.com>
2023-12-05 11:47:32 +0530

    +home icon and gradient home page text

--- src/routes/pages/+layout.svelte
+++ src/routes/pages/+layout.svelte
@@ -1,109 +1,106 @@
-<div class="mx-auto max-w-xl p-5 pb-20">
-  <slot />
-</div>
-<!-- Create a bottom navigatoion bar in svelte and tailwind the bottom nav bar is fixed to the bottom of the screen-->
-<div class="fixed bottom-0 w-full bg-white shadow">
-  <div class="flex justify-around">
-    <a
-      href="/pages/home"
-      class="flex flex-col items-center justify-center py-2"
-    >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        class="h-6 w-6"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="#000000"
-      >
-        <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
-          d="M12 6v6m0 0v6m0-6h6m-6 0H6"
-        ></path>
-      </svg>
-      <span class="text-xs">Home</span>
-    </a>
-    <a
-      href="/pages/attendance"
-      class="flex flex-col items-center justify-center py-2"
-    >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        class="h-6 w-6"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="#000000"
-      >
-        <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
-          d="M12 6v6m0 0v6m0-6h6m-6 0H6"
-        ></path>
-      </svg>
-      <span class="text-xs">Attendance</span>
-    </a>
-    <a
-      href="/pages/timetable"
-      class="flex flex-col items-center justify-center py-2"
-    >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        class="h-6 w-6"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="#000000"
-      >
-        <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
-          d="M12 6v6m0 0v6m0-6h6m-6 0H6"
-        ></path>
-      </svg>
-      <span class="text-xs">Timetable</span>
-    </a>
-    <a
-      href="/pages/profile"
-      class="flex flex-col items-center justify-center py-2"
-    >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        class="h-6 w-6"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="#000000"
-      >
-        <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
-          d="M12 6v6m0 0v6m0-6h6m-6 0H6"
-        ></path>
-      </svg>
-      <span class="text-xs">Profile</span>
-    </a>
+<script>
+import HomeIcon from "../../components/icons/HomeIcon.svelte";
 
-    <a
-      href="/pages/academic_history"
-      class="flex flex-col items-center justify-center py-2"
-    >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        class="h-6 w-6"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="#000000"
+</script>
+<svelte:head>
+  <link
+    rel="stylesheet"
+    href="https://unpkg.com/mono-icons@1.0.5/iconfont/icons.css"
+  />
+</svelte:head>
+
+<div class="min-h-screen dark:bg-slate-900">
+  <div class="mx-auto max-w-xl p-5 pb-20">
+    <slot />
+  </div>
+  <div
+    class="fixed bottom-0 w-full bg-white stroke-black shadow dark:bg-black dark:stroke-white dark:text-white"
+  >
+    <div class="flex justify-around">
+      <a
+        href="/pages/home"
+        class="flex flex-col items-center justify-center py-2"
       >
-        <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
-          d="M12 6v6m0 0v6m0-6h6m-6 0H6"
-        ></path>
-      </svg>
-      <span class="text-xs">Academic History</span>
-    </a>
+        <HomeIcon className="text-xl"/>
+        <span class="text-xs">Home</span>
+      </a>
+      <a
+        href="/pages/attendance"
+        class="flex flex-col items-center justify-center py-2"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+          ></path>
+        </svg>
+        <span class="text-xs">Attendance</span>
+      </a>
+      <a
+        href="/pages/timetable"
+        class="flex flex-col items-center justify-center py-2"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+          ></path>
+        </svg>
+        <span class="text-xs">Timetable</span>
+      </a>
+      <a
+        href="/pages/profile"
+        class="flex flex-col items-center justify-center py-2"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+          ></path>
+        </svg>
+        <span class="text-xs">Profile</span>
+      </a>
+
+      <a
+        href="/pages/academic_history"
+        class="flex flex-col items-center justify-center py-2"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+          ></path>
+        </svg>
+        <span class="text-xs">Academic History</span>
+      </a>
+    </div>
   </div>
 </div>
--- src/routes/pages/home/+page.svelte
+++ src/routes/pages/home/+page.svelte
@@ -1,9 +1,15 @@
 <script lang="ts">
+import HomeIcon from "../../../components/icons/HomeIcon.svelte";
 import TodaysClass from "./todaysClass.svelte";
-
 </script>
 
-<TodaysClass/>
+<h1
+  class="bg-gradient-to-r from-rose-500 to-amber-500 bg-clip-text pb-4 text-5xl font-bold text-transparent"
+>
+  <HomeIcon className="text-3xl"/> Home Page
+</h1>
+
+<TodaysClass />
 
 <!-- create a grid with quick links like acadhistory, attd, profile, timetable  the grid columns are automatic and responseive -->
 <div class="grid grid-cols-2 gap-4 my-10">
