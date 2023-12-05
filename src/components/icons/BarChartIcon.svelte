51d2fb4324be4db1831c7d841aee386a324bf20c
Sarath <vssarathc04@gmail.com>
2023-12-05 12:06:17 +0530

    +attendance icon

---
+++ src/components/icons/BarChartIcon.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+export let className: string = "";
+</script>
+
+<i class="mi mi-bar-chart-alt {className}"></i>
--- src/routes/pages/+layout.svelte
+++ src/routes/pages/+layout.svelte
@@ -1,7 +1,8 @@
 <script>
 import HomeIcon from "../../components/icons/HomeIcon.svelte";
-
+import BarChartIcon from "../../components/icons/BarChartIcon.svelte"
 </script>
+
 <svelte:head>
   <link
     rel="stylesheet"
@@ -21,26 +22,14 @@ import HomeIcon from "../../components/icons/HomeIcon.svelte";
         href="/pages/home"
         class="flex flex-col items-center justify-center py-2"
       >
-        <HomeIcon className="text-xl"/>
+        <HomeIcon className="text-xl" />
         <span class="text-xs">Home</span>
       </a>
       <a
         href="/pages/attendance"
         class="flex flex-col items-center justify-center py-2"
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="h-6 w-6"
-          fill="none"
-          viewBox="0 0 24 24"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M12 6v6m0 0v6m0-6h6m-6 0H6"
-          ></path>
-        </svg>
+        <BarChartIcon className="text-xl"/>
         <span class="text-xs">Attendance</span>
       </a>
       <a
