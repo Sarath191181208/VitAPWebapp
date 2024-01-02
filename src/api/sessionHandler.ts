ecf7867503fa26d88c4ecfb925f6dc9281f67f5d
Sarath <vssarathc04@gmail.com>
2024-01-03 02:04:28 +0530

    cookie aware req

--- src/api/sessionHandler.ts
+++ src/api/sessionHandler.ts
@@ -1,36 +1,76 @@
-import { getCurriculumUrl, getLoginUrl, rootUrl } from "./urls";
+import axios from "axios";
+import { getCurriculumUrl, getRootUrl, getLoginUrl } from "./urls";
 
-let cookie: string = "";
+export const restApi = axios.create({
+  baseURL: getRootUrl(),
+  withCredentials: true,
+});
 
-async function logIn(username: string, password: string) {
-  rootUrl.subscribe(async (value) => {
-    const loginUrl = getLoginUrl(value);
-    const formData = new FormData();
-    formData.append("username", username);
-    formData.append("password", password);
-    const cookieJson = await fetch(loginUrl, {
-      method: "POST",
-      body: formData,
-    });
-    // cons.log({ co kie: c: cookien.headers.get("set-cookie") }) }
-    // cookie = cookieJson.headers.get("set-cookie") || "";
-  });
+restApi.interceptors.request.use(
+  function (config) {
+    config.headers.withCredentials = true; // Sending request with credentials
+    return config;
+  },
+  function (err) {
+    return Promise.reject(err);
+  },
+);
+
+let cookie = {
+  value: "",
+};
+
+async function logIn(
+  rootUrl: string,
+  username: string,
+  password: string,
+): Promise<string> {
+  const loginUrl = getLoginUrl(rootUrl);
+  // use axios to post
+  const formData = new FormData();
+  formData.append("username", username);
+  formData.append("password", password);
+  const resp = await restApi.post(loginUrl, formData);
+  cookie.value = resp.data.cookie;
+  return cookie.value;
 }
 
-async function getCurriculum(roll_no: string) { 
-    rootUrl.subscribe(async (value) => {
-        const curriculumUrl = getCurriculumUrl(value);
-      const formData = new FormData();
-      formData.append("roll_no", roll_no);
-        const curriculumJson = await fetch(curriculumUrl, {
-            method: "POST",
-            body: formData,
-            headers: {
-                "Content-Type": "application/json",
-                "Cookie": cookie,
-            },
-        });
-    });
+export interface CurriculumCourse {
+  basket_title: string;
+  code: string;
+  course_title: string;
+  credits: number;
+  grade: string;
+  is_compulsory: boolean;
+  registered_semester: string;
+  registration_status: boolean;
+  type: string;
 }
 
-export { cookie, logIn, getCurriculum};
+export interface CourseTypes {
+  programme_core: Array<CurriculumCourse>;
+  programme_elective: Array<CurriculumCourse>;
+  university_core: Array<CurriculumCourse>;
+  university_elective: Array<CurriculumCourse>;
+}
+
+export type Curriculum = {
+  credit_info: Array<{ category: string; credits: number }>;
+} & CourseTypes;
+
+async function getCurriculum(
+  rootUrl: string,
+  roll_no: string,
+  _cookie: string,
+): Promise<Curriculum> {
+  console.log({ roll_no, _cookie });
+  const curriculumUrl = getCurriculumUrl(rootUrl);
+  const formData = new FormData();
+  formData.append("roll_no", roll_no);
+  formData.append("cookie", _cookie);
+  const curriculumJson = await restApi.post(curriculumUrl, formData);
+  console.log(curriculumJson.data);
+  return curriculumJson.data;
+}
+
+export { cookie, logIn, getCurriculum };
