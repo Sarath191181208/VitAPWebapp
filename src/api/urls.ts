c735dfdf2f4704f55887adb68f4ae725c6b68cbf
Sarath <vssarathc04@gmail.com>
2024-01-03 02:05:00 +0530

    +get root url fn

--- src/api/urls.ts
+++ src/api/urls.ts
@@ -1,12 +1,14 @@
 import { writable } from "svelte/store";
 
 export const rootUrl = writable<string>("http://127.0.0.1:5000");
+let rootUrlValue = "http://127.0.0.1:5000";
 loadRootUrlFromCache();
 
 rootUrl.subscribe((value) => {
   if (value) {
     localStorage.setItem("rootUrl-V1", JSON.stringify(value));
   }
+  rootUrlValue = value;
 });
 
 function loadRootUrlFromCache() {
@@ -19,4 +21,16 @@ function loadRootUrlFromCache() {
 
 export function getAllDetailsUrl(rootUrl: string) {
   return rootUrl + "/api/v1/alldetails";
+}
+
+export function getRootUrl() {
+  return rootUrlValue;
+}
+
+export function getLoginUrl(rootUrl: string) {
+  return rootUrl + "/api/v1/login";
+}
+
+export function getCurriculumUrl(rootUrl: string) {
+  return rootUrl + "/api/v1/get_curriculum2";
 }
