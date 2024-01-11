058bbb75d7c8c7fd06cd40d483e0e5fa9723d51e
Sarath191181208 <vssarathc04@gmail.com>
2024-01-11 20:49:03 +0530

    exam schedule page

---
+++ src/routes/pages/exam_schedule/+page.svelte
@@ -0,0 +1,20 @@
+<!-- <script lang="ts"> -->
+<!-- import { unamePassword } from "$stores/usernamePassword"; -->
+<!-- import { fetchExamSchedule } from "../../../api/examSchedule"; -->
+<!-- import { rootUrl } from "../../../api/urls"; -->
+<!-- import { getExamScheduleUrl } from "../../../api/urls"; -->
+<!-- const examScheduleUrl = getExamScheduleUrl(rootUrl); -->
+<!-- const unamePassword = $unamePassword; -->
+<!-- </script> -->
+<!---->
+<!-- <div class="dark:text-white"> -->
+<!--   {#if usernameAndPassword === null} -->
+<!--     <h1 class="text-5xl">No username and password found !</h1> -->
+<!--   {:else} -->
+<!--     {#await fetchExamSchedule(examScheduleUrl, unamePassword.username, unamePassword.password)} -->
+<!--     {:then examSchedule} -->
+<!--     {:catch error} -->
+<!--        -->
+<!--     {/await} -->
+<!--   {/if} -->
+<!-- </div> -->
